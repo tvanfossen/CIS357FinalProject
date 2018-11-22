5ee9603fd2a157bd6cc318852db1fe3183a2c386
Unknown <tristan.vanfossen@yahoo.com>
2018-11-22 18:26:41 -0500

    separate branches

--- app/src/main/java/td/warp/com/warptd/GameActivity.java
+++ app/src/main/java/td/warp/com/warptd/GameActivity.java
@@ -1,13 +1,20 @@
 package td.warp.com.warptd;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_game);
     }
 }
